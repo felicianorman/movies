04059dd12b30edafc337fc149a583298574e8363
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-11-09 14:03:56 +0100

    renskrev kod

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,14 @@ import { OmdbResponse } from "./models/OmdbResponse";
 let myWrapper = document.getElementById("wrapper");
 
 fetch("http://omdbapi.com?apikey=416ed51a&s=christmas")
-  //fetch ger oss ett löfte som vi hanterar med .then. om anropet är framgångsrikt kör vi vår funktion response. json() kommer ta svaret vi fick från servern och göra om till json data. json är en asynkron operation, och kommer också ge oss ett löfte.
   .then((response) => response.json())
   .then((data) => {
-    let result = new OmdbResponse(data.totalResults, data.Search);
-    //skapar ett objekt i min klass omdbResponse. data.totalResults motsvarar amount och data.Search motsvarar movies. lagrar i variabeln result så jag kan komma åt dessa enkelt
-    console.log(result);
+    let result = new OmdbResponse(data.Search);
 
-    handleData(result.movies, result.amount); //skickar med movies från min klass.
+    handleData(result.movies); 
   });
 
-function handleData(movies, amount) { //tar emot result.movies som movies
+function handleData(movies) { 
   for (let i = 0; i < movies.length; i++) {
     let container = document.createElement("div");
     let title = document.createElement("h3");
@@ -29,9 +26,5 @@ function handleData(movies, amount) { //tar emot result.movies som movies
     container.classList.add("container");
     myWrapper.appendChild(container);
   }
-
-  console.log(movies);
-  console.log(amount);
-
 }
 
--- src/js/models/omdbresponse.js
+++ src/js/models/omdbresponse.js
@@ -1,10 +1,12 @@
 import { Movie } from "./movie";
 
 export class OmdbResponse {
-    constructor(amount, movies) {
-        this.amount = amount;
+    constructor(movies) {
         this.movies = movies.map((movie) => {
             return new Movie(movie.Title, movie.Poster);
         })
     }
 }
+
+//map loopar värdena som vi vill ha ut från OmdbAPI - Title och Poster
+//Vi returnerar till vår klass Movie där vi döper dem till title och imageUrl
