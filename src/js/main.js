5fb85a68c1cf49d2c354fdb24c18a67c4e1c08a7
Felicia Norman <felicianorman@MacBook-Air-som-tillhor-Felicia.local>
2022-11-09 14:05:18 +0100

    la till kommentarer

--- src/js/main.js
+++ src/js/main.js
@@ -1,21 +1,24 @@
 import { OmdbResponse } from "./models/OmdbResponse";
 
+//Hämtar <div> från min HTML
 let myWrapper = document.getElementById("wrapper");
 
 fetch("http://omdbapi.com?apikey=416ed51a&s=christmas")
-  .then((response) => response.json())
+  .then((response) => response.json()) 
   .then((data) => {
-    let result = new OmdbResponse(data.Search);
+    let result = new OmdbResponse(data.Search); //Skapar objekt från min klass OmdbResponse. Det är Search från Omdb API. Lagrar i en variabel för enkel åtkomst
 
-    handleData(result.movies); 
+    handleData(result.movies); //Skickar med variabeln result och movies från min OmdbResponse till funktionen som parameter
   });
 
-function handleData(movies) { 
-  for (let i = 0; i < movies.length; i++) {
+function handleData(movies) {  //tar emot result.movies och döper till movies
+  //loopar igenom och skapar HTML
+  for (let i = 0; i < movies.length; i++) { 
     let container = document.createElement("div");
     let title = document.createElement("h3");
     let img = document.createElement("img");
   
+    //title och imgUrl från min Movie klass
     title.innerHTML = movies[i].title;
     container.appendChild(title);
 
--- src/js/models/movie.js
+++ src/js/models/movie.js
@@ -1,4 +1,5 @@
 export class Movie {
+  //Skapar titel och imageUrl, där jag kan visa omdbApi's Title och Poster
   constructor(title, imageUrl) {
     this.title = title;
     this.imageUrl = imageUrl;
